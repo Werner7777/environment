8baef9fbfe124de0e4a1654911edd9753df69f03
werner <werner@804612726@qq.com>
2021-11-06 16:20:36 +0800

    edition1.1

--- environment.py
+++ environment.py
@@ -1,9 +1,55 @@
 from builtins import range, len
 import random
-
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+##lane information   自车期望车速？？？？？
+class Highway:
+    def __init__(self, figure1=0, figure2=0):  # 初始化
+        self.carsnumber = 1 + 6    # number of surrounding Cars
+        self.Vel = np.empty([1, self.carsnumber])  # 车速km/h，顺序为自车、左前车、正前车、右前车、左后车、正后车、右后车
+        self.TarVel = np.empty([1, self.carsnumber])  # 目标车速km/h，顺序为自车、左前车、正前车、右前车、左后车、正后车、右后车
+        self.PosX = np.empty([1, self.carsnumber])  # 全局纵向位置m，顺序上同
+        self.PosY = np.empty([1, self.carsnumber])  # 全局横向位置m，顺序上同
+        self.TarVel = np.empty([1, self.carsnumber - 1])
+        self.EgoTarVel = np.empty(1, 1)
+        self.EgoLaneID = 1 # 自车所处车道ID
+        self.LaneWidth = 3.5
+        self.WhichLane = [0, 1, 2]
+        self.LaneRange = np.arange(-200., 200., 0.01)  # range of lane
+        f1 = figure1  # 是否画场景俯视图标志位
+        f2 = figure2  # 是否画观测数据列表图标志位
+        # 画换道路线
+
+    def reset(self):
+        # initialize lane data
+        secureDistance = 10.  # collision distance centric Car
+        self.Vel[0] = 80 / 3.6  # initialize Car's velocity(unit:metre per second)
+        self.EgoTarVel = 80 / 3.6  # initialize Car's expected velocity
+        judge = True  # a viriation to judge collision
+        # initialize car's data
+        self.PosX[-1] =0.
+        self.PosY[-1] = self.WhichLane[1] * self.LaneWidth - 0.5 * self.LaneWidth  # [lateral location, whichLane]
+        self.PosY = whichLane / 2 * laneWide - 0.5 * laneWide
+        while judge == True:
+            for i in range(0, self.carsnumber - 1):
+
+                if i < 3:
+                    self.PosX[i] = random.uniform(self.LaneRange[0], self.PosX[-1])
+                    self.Vel[i] = random.uniform(50, 90) / 3.6  # initialize surrounding Car's velocity
+                    self.TarVel[i] = random.uniform(50,
+                                                            90) / 3.6  # initialize surrounding Car's expected velocity
+                else:
+                    self.PosX[i] = random.uniform(self.PosX[-1], self.LaneRange[-1])
+                    self.Vel[i] = random.uniform(50, 90) / 3.6  # initialize surrounding Car's velocity
+                    self.TarVel[i] = random.uniform(50,
+                                                            90) / 3.6  # initialize surrounding Car's expected velocity
+            judge = done(np.vstack([surroundingCarsLocation, CarLocation]), secureDistance)
+        plotInit(CarLocation, surroundingCarsNumber, surroundingCarsLocation, laneInformation, fig)
+        carsInformation = [CarLocation, vCar, vExpCar, surroundingCarsNumber,
+                           surroundingCarsLocation, vSurroundingCars,
+                           vExpSurroundingCars]  # collect information about car and surrounding cars
+
 
 # judge collision
 def done(Location, secureDistance):
@@ -45,7 +91,7 @@ def initialize(laneInformation):
                        surroundingCarsLocation, vSurroundingCars, vExpSurroundingCars]  # collect information about car and surrounding cars
     return carsInformation
 def plotInit(CarLocation, surroundingCarsNumber, surroundingCarsLocation, laneInformation, fig):
-    laneRange = np.arange(CarLocation[0]-200., CarLocation[0]+200., 0.01)
+    laneRange = np.arange(-200., 200., 0.01)
     laneWide = laneInformation[1]
     whichLane = laneInformation[2]
     #ax = fig.add_subplot(111)
@@ -53,11 +99,11 @@ def plotInit(CarLocation, surroundingCarsNumber, surroundingCarsLocation, laneIn
         plt.plot(laneRange, np.zeros(len(laneRange)), 'r--')
         plt.plot(laneRange, np.ones(len(laneRange)) * laneWide * i, 'r--')
     for i in range(0, surroundingCarsNumber):
-        ax.add_patch(patches.Rectangle((surroundingCarsLocation[i] - 1), 5, 2, 0, color="blue"))
-    ax.add_patch(patches.Rectangle((CarLocation - 1),5 ,2,0,color="red"))
-    plt.xlabel("X-AXIS")
-    plt.ylabel("Y-AXIS")
-    plt.title("PLOT-1")
+        ax.add_patch(patches.Rectangle((surroundingCarsLocation[i]-CarLocation), 5, 2, 0, color="blue"))
+    ax.add_patch(patches.Rectangle(([0, 0]),5 ,2,0,color="red"))
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.title("output")
     plt.xlim([laneRange[0], laneRange[-1]])
     fig.show()
     plt.pause(0.003)
@@ -119,7 +165,7 @@ whichLane = [0, 1, 2]  # number the lane
 laneInformation = [laneRange, laneWide, whichLane]
 secureDistance = 10
 
-fig= plt.figure()
+fig= plt.figure(num=1, figsize=(8, 50/(laneRange[-1]*2)*len(whichLane)*laneWide))
 ax = fig.add_subplot(111)
 carsInformation = initialize(laneInformation)
 
