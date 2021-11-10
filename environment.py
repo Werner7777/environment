52cdfc288239e84a509069f3b3bab4acefd8f94e
werner <werner@804612726@qq.com>
2021-11-10 18:20:12 +0800

    edition 2.4

--- environment.py
+++ environment.py
@@ -3,7 +3,10 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
-## lane information
+import math
+# lane information
+
+np.warnings.filterwarnings('ignore', category=np.VisibleDeprecationWarning)
 class Highway:
 
     def __init__(self, figure1=0, figure2=0):  # 初始化
@@ -14,7 +17,7 @@ class Highway:
         self.PosY = np.empty([self.carsnumber, 1])  # 全局横向位置m，顺序上同
         self.TarVel = np.empty([self.carsnumber, 1])
         self.EgoTarVel = np.empty([1, 1])
-        self.EgoLaneID = 1 # 自车所处车道ID
+        self.EgoLaneID = 1  # 自车所处车道ID
         self.LaneWidth = 3.5
         self.WhichLane = [0, 1, 2]
         self.LaneRange = np.arange(-200., 200., 0.01)
@@ -27,12 +30,12 @@ class Highway:
     secureDistance = 10.
 
     def plot1(self):
-        if (self.f1 == 1):
+        if self.f1 == 1:
             for i in np.hstack([-1, self.WhichLane]):
                 plt.plot(self.LaneRange, np.ones(len(self.LaneRange)) * self.LaneWidth * i, 'r--')
             for i in range(1, self.carsnumber):
                 self.ax.add_patch(patches.Rectangle(([self.PosX[i]-self.PosX[0], self.PosY[i] - self.LaneWidth/4]), 5, 2, 0, color="blue"))
-            self.ax.add_patch(patches.Rectangle(([0, 0 + self.LaneWidth/4]), 5, 2, 0, color="red"))
+            self.ax.add_patch(patches.Rectangle(([0, self.PosY[0] - self.LaneWidth/4]), 5, 2, 0, color="red"))
             plt.xlabel("X")
             plt.ylabel("Y")
             plt.title("output")
@@ -43,12 +46,12 @@ class Highway:
 
     def reset(self):
         # initialize lane data
-          # collision distance centric Car
+        # collision distance centric Car
         self.Vel[0] = 80 / 3.6  # initialize Car's velocity(unit:metre per second)
         self.EgoTarVel = 80 / 3.6  # initialize Car's expected velocity
         judge = True  # a viriation to judge collision
         # initialize car's data
-        self.PosX[0] =0.
+        self.PosX[0] = 0.
         self.PosY[0] = self.WhichLane[1] * self.LaneWidth - 0.5 * self.LaneWidth  # [lateral location, whichLane]
         while judge == True:
             for i in range(1, self.carsnumber):
@@ -64,7 +67,7 @@ class Highway:
                     self.Vel[i] = random.uniform(50, 90) / 3.6  # initialize surrounding Car's velocity
                     self.TarVel[i] = random.uniform(50, 90) / 3.6  # initialize surrounding Car's expected velocity
             Location = np.hstack([self.PosX, self.PosY])
-            judge = self.done(Location, self.secureDistance)
+            judge = self.done2(Location, self.secureDistance)
         self.plot1()
 
     def run(self, turn):  # 0 is keep lane, 1 is turn left , 2 is turn right
@@ -72,13 +75,6 @@ class Highway:
         # longitudinal control
         a = np.zeros([self.carsnumber, 1])
         place = self.sort(turn)
-        if (self.done(np.hstack([self.PosX[:3:2], self.PosY[:3:2]]), self.secureDistance * 1.5)):
-            a[0] = -30
-        else:
-            if(self.Vel[0] < self.TarVel[0]):
-                a[0] = 20
-            else:
-                a[0] = 0
         # target velocity
         for i in range(0, self.carsnumber):
             if (self.Vel[i] < self.TarVel[i]):
@@ -108,17 +104,43 @@ class Highway:
         self.Vel[0] = self.Vel[0] + a[0] * dt
         self.Vel[1:] = self.Vel[1:] + a[1:] * dt
         self.PosX[1:] = self.Vel[1:] * dt + a[1:] * 0.5 * np.power(dt, 2) + self.PosX[1:]
+        dx=self.Vel[0] * dt + a[0] * 0.5 * np.power(dt, 2)
+        self.lateral(turn, dx, dt)
 
         # judge collision and plot
         self.plot1()
         Location = np.hstack([self.PosX, self.PosY])
-        judge = self.done(Location, self.secureDistance)
+        judge = self.done2(Location, self.secureDistance)
         reward = 0
         next_s = [self.PosX, self.PosY]
         return next_s, reward, judge
 
+    def lateral(self, turn, xc, Ts):
+        yc=self.PosY[0]
+        L=180
+        pi=3.141592653589793
+        if(turn==1): #turn left
+            yc = yc + 50*(self.LaneWidth / (2 * pi) * (2 * pi * (xc + Ts * self.Vel[0]) / L + math.sin(2 * pi * (xc + Ts * self.Vel[0]) / L - pi)) - self.LaneWidth / (
+                 2 * pi) * (2 * pi * xc / L + math.sin(2 * pi * xc / L - pi)))
+        elif(turn==2): #tuen right
+            yc = yc - (self.LaneWidth / (2 * pi) * (2 * pi * (xc + Ts * self.Vel[0]) / L + math.sin(2 * pi * (xc + Ts * self.Vel[0]) / L - pi)) - self.LaneWidth / (
+                    2 * pi) * (2 * pi * xc / L + math.sin(2 * pi * xc / L - pi)))
+        self.PosY[0] = yc
+        self.EgoLaneID = int(1.001 + self.PosY[0]//self.LaneWidth)
+        if self.EgoLaneID != 1:
+            kk=0
+
+
     # judge collision
     def done(self, Location, secureDistance):
+        judge = False
+        for i in range(0 , np.size(Location, 0)):
+            for j in range(i + 1, np.size(Location, 0)):
+                if np.linalg.norm([Location[i] - Location[j]]) <= secureDistance :
+                    judge = True
+        return judge
+
+    def done2(self, Location, secureDistance):
         judge = False
         for i in range(0 , np.size(Location, 0)):
             for j in range(i + 1, np.size(Location, 0)):
@@ -129,7 +151,7 @@ class Highway:
     def sort(self, lane):
         place = 1
         for i in range(1, self.carsnumber - 1):
-            if ((lane * self.LaneWidth/2 == self.PosY[i]) & (self.PosX[0] < self.PosX[i])):
+            if (((lane - 0.5) * self.LaneWidth == self.PosY[i]) & (self.PosX[0] < self.PosX[i])):
                 place += 1
         return place
     '''
@@ -155,7 +177,7 @@ for i in range(episodes):
 
     # 每步运行
     for j in range(step):
-        next_s, reward, done = ev.run(0)
+        next_s, reward, done = ev.run(1)
         if done:
             score_list.append(score)
             break
